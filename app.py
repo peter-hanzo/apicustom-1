ca6dbe1d392c6379d66f98c0f186341a8001f0f9
jscore <yevmail@gmail.com>
2023-06-27 14:14:08 +0200

    Update app.py

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ import requests
 from werkzeug.utils import secure_filename
 import os
 import ffmpeg
+from scipy.spatial import distance
 
 
 def create_app():
@@ -29,3 +30,20 @@ def homepage():
 @app.route('/hello', methods=['GET'])
 def hello():
     return "Hello"
+
+@app.route('/get_similar', methods=['POST'])
+def cosine_similarity():
+    data = request.json
+    query_vector = data['query_vector']
+    vector_text_pairs = data['vectors']
+
+    # Extract embeddings and their corresponding texts
+    vectors = [pair['embeddings'] for pair in vector_text_pairs]
+    texts = [pair['text'] for pair in vector_text_pairs]
+
+    # Calculate cosine similarity for each vector
+    # Return the index of the most similar vector
+    most_similar_index = max(range(len(vectors)), key=lambda index: 1 - distance.cosine(query_vector, vectors[index]))
+
+    return jsonify({'most_similar_text': texts[most_similar_index]})
+
