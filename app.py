44852430a1eb7a0e08c6c5f6279106a562b530ad
Alexander <33152397+boumer7@users.noreply.github.com>
2023-07-30 22:46:56 +0300

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,14 @@ from flask import Flask, request, jsonify, send_file
 import os
 import ffmpeg
 import requests
+import psycopg2
+import os
+
+DB_HOST = os.environ.get('PGHOST')
+DB_PORT = os.environ.get('PGPORT')
+DB_USER = os.environ.get('PGUSER')
+DB_PASSWORD = os.environ.get('PGPASSWORD')
+DB_DATABASE = os.environ.get('PGDATABASE')
 
 def create_app():
     app = Flask(__name__, static_folder='uploads', static_url_path='/uploads')
@@ -50,6 +58,34 @@ def trim_video_to_mp3():
     ffmpeg.input(video_filepath, ss=start_time, to=end_time).output(trimmed_filepath, audio_bitrate=audio_bitrate).run()
 
     return send_file(trimmed_filepath, as_attachment=True)
+
+@app.route('/add_to_db', methods=['POST'])
+def add_to_db():
+    data = request.get_json()
+    item_name = data['item_name']
+    item_description = data['item_description']
+
+    try:
+        conn = psycopg2.connect(
+            host=DB_HOST,
+            port=DB_PORT,
+            user=DB_USER,
+            password=DB_PASSWORD,
+            database=DB_DATABASE
+        )
+        cursor = conn.cursor()
+
+        query = "INSERT INTO items (name, description) VALUES (%s, %s);"
+        cursor.execute(query, (item_name, item_description))
+        conn.commit()
+
+        cursor.close()
+        conn.close()
+
+        return jsonify({"status": "success", "message": "Item added to the database successfully."})
+
+    except psycopg2.Error as e:
+        return jsonify({"status": "error", "message": str(e)})
 
 if __name__ == '__main__':
     app.run()
