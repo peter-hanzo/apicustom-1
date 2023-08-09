22c680e3380113c1936e26fdc75fdbe7ae7cce11
Alexander <33152397+boumer7@users.noreply.github.com>
2023-08-09 20:11:34 +0300

    Update app.py

--- app.py
+++ app.py
@@ -3,8 +3,9 @@ import uuid
 from flask import Flask, request, jsonify, Response, send_file
 import os
 import ffmpeg
-from pytube import YouTube
+from pytube import YouTube, Stream
 import os
+from pydub import AudioSegment
 
 DB_HOST = os.environ.get('PGHOST')
 DB_PORT = os.environ.get('PGPORT')
@@ -22,26 +23,8 @@ def create_app():
 
 app = create_app()
 
-def trim_and_convert_video(video_url, start_time, end_time, audio_bitrate, output_format):
-    # Download the video using pytube
-    yt = YouTube(video_url)
-    video = yt.streams.filter(progressive=True, file_extension='mp4').first()
-    video_filepath = os.path.join(app.config['UPLOAD_FOLDER'], f"{uuid.uuid4()}.mp4")
-    video.download(output_path=app.config['UPLOAD_FOLDER'], filename=video_filepath)
-
-    trimmed_filename = f"{uuid.uuid4()}.{output_format}"
-    trimmed_filepath = os.path.join(app.config['UPLOAD_FOLDER'], trimmed_filename)
-
-    if output_format == 'mp3':
-        ffmpeg.input(video_filepath, ss=start_time, to=end_time).output(trimmed_filepath, audio_bitrate=audio_bitrate).run()
-    elif output_format == 'mp4':
-        ffmpeg.input(video_filepath, ss=start_time, to=end_time).output(trimmed_filepath).run()
-    else:
-        raise ValueError("Invalid output_format. Supported formats: 'mp3', 'mp4'")
-
-    os.remove(video_filepath)
-
-    return trimmed_filepath
+def download_youtube_video(video_url):
+    return YouTube(video_url)
 
 @app.route('/', methods=['GET'])
 def homepage():
@@ -51,74 +34,149 @@ def homepage():
 def hello():
     return "Hello"
 
-@app.route('/trim_video_to_mp3', methods=['POST', 'GET'])
-def trim_video_to_mp3():
+def download_audio(audio_url, audio_format):
+    audio_filename = f"{uuid.uuid4()}.{audio_format}"
+    audio_filepath = os.path.join(app.config['UPLOAD_FOLDER'], audio_filename)
+    
+    # Download the audio file
+    subprocess.run(["wget", audio_url, "-O", audio_filepath], check=True)
+    
+    return audio_filepath
+
+def convert_to_mp3(audio_filepath, mp3_bitrate):
+    audio_format = audio_filepath.split('.')[-1].lower()
+    audio = AudioSegment.from_file(audio_filepath, format=audio_format)
+    
+    mp3_filepath = os.path.join(app.config['UPLOAD_FOLDER'], f"{uuid.uuid4()}.mp3")
+    audio.export(mp3_filepath, format="mp3", bitrate=mp3_bitrate)
+    
+    os.remove(audio_filepath)
+    
+    return mp3_filepath
+
+def download_clip(video_url):
+    return YouTube(f"https://www.youtube.com/{video_url[21:]}")
+
+def trim_video(video_stream: Stream, start_time, end_time):
+    video_stream.download(output_path=app.config['UPLOAD_FOLDER'])
+    video_filepath = os.path.join(app.config['UPLOAD_FOLDER'], f"{video_stream.video_id}.mp4")
+    
+    trimmed_filepath = os.path.join(app.config['UPLOAD_FOLDER'], f"{uuid.uuid4()}.mp4")
+    
+    ffmpeg.input(video_filepath, ss=start_time, to=end_time).output(trimmed_filepath).run()
+    os.remove(video_filepath)
+    
+    return trimmed_filepath
+
+def download_subtitles(video_url):
+    yt = YouTube(video_url)
+    transcript = yt.captions.get_by_language_code('en').generate_srt_captions()
+    
+    subtitles_filepath = os.path.join(app.config['UPLOAD_FOLDER'], f"{uuid.uuid4()}.srt")
+    
+    with open(subtitles_filepath, 'w') as f:
+        f.write(transcript)
+    
+    return subtitles_filepath
+
+@app.route('/download_video', methods=['POST', 'GET'])
+def download_video():
+    if request.method == 'POST':
+        data = request.form
+        video_url = data['video_url']
+    elif request.method == 'GET':
+        video_url = request.args.get('video_url')
+    
+    try:
+        video = download_youtube_video(video_url)
+        video_stream = video.streams.filter(progressive=True, file_extension='mp4').first()
+        
+        return send_file(video_stream.url, as_attachment=True)
+
+    except Exception as e:
+        return jsonify({"status": "error", "message": str(e)})
+
+@app.route('/download_audio', methods=['POST', 'GET'])
+def download_audio_route():
+    if request.method == 'POST':
+        data = request.form
+        audio_url = data.get('audio_url')
+        audio_format = audio_url.split('.')[-1].lower()
+        mp3_bitrate = data.get('mp3_bitrate', '128k')
+    elif request.method == 'GET':
+        audio_url = request.args.get('audio_url')
+        audio_format = audio_url.split('.')[-1].lower()
+        mp3_bitrate = request.args.get('mp3_bitrate', '128k')
+
+    try:
+        if audio_format in ['ogg', 'm4a', 'webm', 'mp3']:
+            audio_filepath = download_audio(audio_url, audio_format)
+            
+            if audio_format != 'mp3':
+                mp3_filepath = convert_to_mp3(audio_filepath, mp3_bitrate)
+                return send_file(mp3_filepath, as_attachment=True)
+            else:
+                return send_file(audio_filepath, as_attachment=True)
+        else:
+            return jsonify({"status": "error", "message": "Invalid audio format. Supported formats: 'ogg', 'm4a', 'webm', 'mp3'"})
+
+    except Exception as e:
+        return jsonify({"status": "error", "message": str(e)})
+
+@app.route('/download_clip', methods=['POST', 'GET'])
+def download_clip_route():
+    if request.method == 'POST':
+        data = request.form
+        clip_url = data['clip_url']
+    elif request.method == 'GET':
+        clip_url = request.args.get('clip_url')
+
+    try:
+        clip = download_clip(clip_url)
+        video_stream = clip.streams.filter(progressive=True, file_extension='mp4').first()
+        
+        return send_file(video_stream.url, as_attachment=True)
+
+    except Exception as e:
+        return jsonify({"status": "error", "message": str(e)})
+
+@app.route('/trim_video', methods=['POST', 'GET'])
+def trim_video_route():
     if request.method == 'POST':
         data = request.form
         video_url = data['video_url']
         start_time = data['start_time']
         end_time = data['end_time']
-        audio_bitrate = data.get('audio_bitrate', '128k')
-        output_format = data.get('output_format', 'mp3')  # Default to mp3
     elif request.method == 'GET':
         video_url = request.args.get('video_url')
         start_time = request.args.get('start_time')
         end_time = request.args.get('end_time')
-        audio_bitrate = request.args.get('audio_bitrate', '128k')
-        output_format = request.args.get('output_format', 'mp3')  # Default to mp3
 
     try:
-        # Use streaming to send the trimmed video directly to the client
-        trimmed_filepath = trim_and_convert_video(video_url, start_time, end_time, audio_bitrate, output_format)
+        video = download_youtube_video(video_url)
+        video_stream = video.streams.filter(progressive=True, file_extension='mp4').first()
+        
+        trimmed_filepath = trim_video(video_stream, start_time, end_time)
+        
         return send_file(trimmed_filepath, as_attachment=True)
 
     except Exception as e:
         return jsonify({"status": "error", "message": str(e)})
-        
-@app.route('/add_to_db', methods=['POST', 'GET'])
-def add_to_db():
+
+@app.route('/download_subtitles', methods=['POST', 'GET'])
+def download_subtitles_route():
     if request.method == 'POST':
-        data = request.get_json()
+        data = request.form
+        video_url = data['video_url']
     elif request.method == 'GET':
-        data = request.args
+        video_url = request.args.get('video_url')
 
-    item_name = data.get('item_name')
-    item_description = data.get('item_description')
+    try:
+        subtitles_filepath = download_subtitles(video_url)
+        return send_file(subtitles_filepath, as_attachment=True)
 
-    if item_name and item_description:
-        try:
-            conn = psycopg2.connect(
-                host=DB_HOST,
-                port=DB_PORT,
-                user=DB_USER,
-                password=DB_PASSWORD,
-                database=DB_DATABASE
-            )
-            cursor = conn.cursor()
-
-            query = "INSERT INTO items (name, description) VALUES (%s, %s);"
-            cursor.execute(query, (item_name, item_description))
-            conn.commit()
-
-            cursor.close()
-            conn.close()
-
-            if request.method == 'POST':
-                return jsonify({"status": "success", "message": "Item added to the database successfully."})
-            elif request.method == 'GET':
-                return "Item added to the database successfully."
-
-        except psycopg2.Error as e:
-            if request.method == 'POST':
-                return jsonify({"status": "error", "message": str(e)})
-            elif request.method == 'GET':
-                return "Error: " + str(e)
-
-    else:
-        if request.method == 'POST':
-            return jsonify({"status": "error", "message": "Item name and description must be provided."})
-        elif request.method == 'GET':
-            return "Error: Item name and description must be provided."
+    except Exception as e:
+        return jsonify({"status": "error", "message": str(e)})
 
 if __name__ == '__main__':
     app.run()
